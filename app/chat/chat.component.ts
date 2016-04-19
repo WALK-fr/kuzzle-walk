d89bac1242b0656b557cfcc10cbefadc5a39de2e
ScreamZ <andreas.hanss67@gmail.com>
2016-04-19 22:03:14 +0200

    Init chat component

---
+++ app/chat/chat.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "angular2/core";
+
+/**
+ * This component represent the chatroom of the travel.
+ */
+@Component({
+    'selector': 'chat'
+})
+export class ChatComponent {
+
+}
