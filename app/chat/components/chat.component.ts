2d7e3b29210757a3f3cf560a6a5c6c5540f098bf
ScreamZ <andreas.hanss67@gmail.com>
2016-05-08 22:00:46 +0200

    Init chatmessage

--- app/chat/components/chat.component.ts
+++ app/chat/components/chat.component.ts
@@ -4,8 +4,29 @@ import {Component} from "angular2/core";
  * This components represent the chatroom of the travel.
  */
 @Component({
-    'selector': 'chat'
+    selector: 'chat',
+    styles: [``],
+    templateUrl: "app/chat/components/chat.component.html"
 })
 export class ChatComponent {
+    message:string;
 
+    /**
+     * When an user hit ENTER, the message is propagated.
+     *
+     * @param event
+     */
+    publishMessage(event:KeyboardEvent) {
+        event.stopPropagation();
+        const KEY_ENTER = 13;
+        var key = event.which || event.keyCode;
+
+        if (key === KEY_ENTER) {
+            // TODO : Do kuzzle call to dispatch message
+            console.log(this.message);
+            this.message = "";
+
+            // TODO add message to the panel upon kuzzle success response
+        }
+    }
 }
--- app/travel/components/travel.component.ts
+++ app/travel/components/travel.component.ts
@@ -2,13 +2,14 @@ import {Component, AfterViewInit} from 'angular2/core';
 
 import {MapComponent, PoiFormComponent} from '../../map/index';
 import {NavbarComponent} from './navbar.component';
+import {ChatComponent} from "../../chat/components/chat.component";
 
 // this is used to accept jquery token at compilation time
 declare var $: any;
 
 @Component({
     templateUrl: 'app/travel/components/travel.component.html',
-    directives: [MapComponent, PoiFormComponent, NavbarComponent]
+    directives: [MapComponent, PoiFormComponent, NavbarComponent, ChatComponent]
 })
 export class TravelComponent implements AfterViewInit {
 
