45dbcce093fd711b756bb8d003b4242e80d6f261
Pierre Cavalet <cavalet.pierre@gmail.com>
2016-05-13 18:35:37 +0200

    filling the travel selector view with his component properties

--- app/travel/components/travel-selector.component.ts
+++ app/travel/components/travel-selector.component.ts
@@ -1,9 +1,22 @@
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 @Component({
     selector: 'travel-selector',
     templateUrl: 'app/travel/components/travel-selector.component.html'
 })
 export class TravelSelectorComponent {
+    @Input() mainLocationName: string;
+    @Input() stepNames: string[];
+    // TODO: define how we handle the current step and what type we use
     
+    constructor() {
+        this.mainLocationName = "France";
+        this.stepNames = [
+            "Paris",
+            "Marseille",
+            "Lyon",
+            "Nice"
+        ];
+    }
+
 }
