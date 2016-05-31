d38bdac722301ca8ec960cdbbd1089cd0c125210
michel-p <michel.parreno@mines-ales.org>
2016-05-31 22:49:02 +0200

    notes and reminder system work in progress

--- app/notes/components/notes.component.ts
+++ app/notes/components/notes.component.ts
@@ -12,7 +12,42 @@ declare var $:any;
     styleUrls: ['app/notes/components/notes.component.css']
 })
 export class NotesComponent{
+    
+    @Input() allNotes:[any];
 
+    constructor(){
+        //simulate kuzzle call
+        this.allNotes = [
+            {"name":"Paris", "items":[{"name":"note1", "done":false} , {"name":"note2", "done":false}, {"name":"note3", "done":false}]},
+            {"name":"Deauville", "items":[{"name":"note1", "done":false} , {"name":"note2", "done":false}, {"name":"note3", "done":false}]},
+            {"name":"Bibi", "items":[{"name":"note1", "done":false} , {"name":"note2", "done":false}, {"name":"note3", "done":false}]},
+            {"name":"DeBi", "items":[{"name":"note1", "done":false} , {"name":"note2", "done":false}, {"name":"note3", "done":false}]},
+        ];
+    }
+
+    addNewNote(){
+        
+    }
+    
+    addNewItem(){
+        
+    }
+    
+    markAsDone($event:any, note:any, item:any){
+        var noteIndex = this.allNotes.indexOf(note);
+        var itemIndex = this.allNotes[noteIndex].items.indexOf(item);
+
+        this.allNotes[noteIndex].items[itemIndex].done = true;
+        $($event.target).parents('li').addClass("green","lighten-1");
+    }
+    
+    cancelItem(note:any, item:any){
+        var noteIndex = this.allNotes.indexOf(note);
+        var itemIndex = this.allNotes[noteIndex].items.indexOf(item);
+
+        this.allNotes[noteIndex].items.splice(itemIndex, 1);
+    }
+    
     displayModal() {
         $('#tp-notes').openModal();
     }
