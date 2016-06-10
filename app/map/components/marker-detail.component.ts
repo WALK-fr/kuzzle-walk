5ed3fc87edb3fad59b305c25a8cdacb32296c320
michel-p <michel.parreno@mines-ales.org>
2016-06-11 00:00:23 +0200

    marker detail ok but will need refactor once markers will be cleaned up

--- app/map/components/marker-detail.component.ts
+++ app/map/components/marker-detail.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
 import {TravelMarker} from "../models/travel-marker.model";
+import {Travel} from "../../travel/models/travel.model";
 
 @Component({
     selector: 'marker-detail',
     templateUrl: 'app/map/components/marker-detail.component.html',
     styleUrls: ['app/map/components/marker-detail.component.css']
 })
-export class MarkerDetailComponent {
-
+export class MarkerDetailComponent{
+    @Input() travel: Travel;
     @Input() marker: TravelMarker;
     @Output('back-to-list') backToListEvent = new EventEmitter();
+
 
     /**
      * Remove the information of a marker and display the default panel view
